468f96cf09be1c04fc76433ac14a86a5377c0511
NaniasNic <ananiasdias0@gmail.com>
2023-03-20 13:36:22 -0300

    Articles: ajusted images

--- src/components/Articles/index.tsx
+++ src/components/Articles/index.tsx
@@ -1,5 +1,9 @@
 import * as S from "./styles";
 
+import Tunza from "../../assets/img-barco.jpg";
+import Timor from "../../assets/img-teste.jfif";
+import Loombok from "../../assets/img-oceano.jfif";
+
 export function Articles() {
     const variantsLogo = {
         first: { opacity: 0, y: 70 },
@@ -23,10 +27,10 @@ export function Articles() {
                 <h1>Latest articles</h1>
 
                 <h3>our most recent posts</h3>
-                
+
                 <hr />
             </S.LogoArticles>
-            
+
             <S.CardsContainer
                 initial="first"
                 whileInView="last"
@@ -34,29 +38,47 @@ export function Articles() {
                 viewport={{ once: true, amount: 0.8 }}
                 transition={{ type: "spring", stiffness: 30, delay: 1.3 }}
             >
-                {
-                    [
-                        { cardId: "Card one", back: ("../../assets/img-barco.jpg") ,firstTitle: "Tunza", secondTitle: "Rugged Port" },
-                        { cardId: "Card two",  back: ("../../assets/img-teste.jfif") ,firstTitle: "Timor", secondTitle: "Untouched Beaches" },
-                        { cardId: "Card three", back: ("../../assets/img-oceano.jfif") ,firstTitle: "Lombook", secondTitle: "Endless Lava" },
-                    ].map(cardsName => (
-                        <S.Card
-                            key={cardsName.cardId}
-                        >
-                            <div className="img">                                
-                                <img src={cardsName.back} alt="" />
-                            </div>
+                <S.Card>
+                    <div className="img">
+                        <img src={Tunza} alt="Tunza" />
+                    </div>
 
-                            <div className="text">                                
-                                <h1>{cardsName.firstTitle}</h1>
+                    <div className="text">
+                        <h1>Tunza</h1>
 
-                                <h3>{cardsName.secondTitle}</h3>
+                        <h3>Rugged Port</h3>
 
-                                <hr />
-                            </div>
-                        </S.Card>
-                    ))
-                }
+                        <hr />
+                    </div>
+                </S.Card>
+                
+                <S.Card>
+                    <div className="img">
+                        <img src={Timor} alt="Timor" />
+                    </div>
+
+                    <div className="text">
+                        <h1>Timor</h1>
+
+                        <h3>Untouched Beaches</h3>
+
+                        <hr />
+                    </div>
+                </S.Card>
+                
+                <S.Card>
+                    <div className="img">
+                        <img src={Loombok} alt="Lombook" />
+                    </div>
+
+                    <div className="text">
+                        <h1>Lombook</h1>
+
+                        <h3>Endless Lava</h3>
+
+                        <hr />
+                    </div>
+                </S.Card>
             </S.CardsContainer>
         </S.Container>
     )
